b48907e018936393a0d262f6738fd09879d24922
Mahmoud Bakr <mbakr6821@gmail.com>
2020-08-24 14:19:52 +0200

    add create method and new.html.erb file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,17 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
-
 	def new
+		@article = Article.new
 	end
-	
+
+	def create
+		@article = Article.new(params[:article])
+		@article.save
+		redirect_to article_path(@article)
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
 end
