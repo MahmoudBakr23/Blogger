4e3546775a741c25564b077d42c254414076057f
Mahmoud Bakr <mbakr6821@gmail.com>
2020-08-24 14:27:53 +0200

    modify index, show erb files to show the id of each article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,10 +13,13 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(params[:article])
+		@article = Article.new(article_params)
 		@article.save
 		redirect_to article_path(@article)
 	end
+
+
+	private
 
 	def article_params
 		params.require(:article).permit(:title, :body)
