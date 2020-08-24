a711f4f7988c347158b85f0b3cc64984e794925a
Vitalie Melnic <vmwhoami@gmail.com>
2020-08-24 16:42:09 +0300

    Add flash messages and change the root of our blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,12 +16,27 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 		@article.save
+		flash.notice = "a  '#{@article.title}' article was created!"
 		redirect_to article_path(@article)
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "The  '#{@article.title}' article was Updated!"
+	  
+		redirect_to article_path
 	end
 
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
+		flash.notice = "The  '#{@article.title}' article was deleted!"
 		redirect_to articles_path
 	end
 	
